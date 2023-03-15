163e1715323fc0e0ccfa1c4e0c877bbcab767625
Pedro Gomes <p.f.q.gomes@gmail.com>
2023-03-15 18:43:48 +0000

    Added TreeSitter key mappings.

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,5 +2,14 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = { "lua", "rust", "json", "toml", "c", "cpp" },
+    incremental_selection = {
+      enabled = true,
+      keymaps = {
+        init_selection = "<C-Space>",
+        node_incremental = "<C-Space>",
+        scope_incremental = "<nop>",
+        node_decremental = "<Backspace>",
+      }
+    },
   },
 }
