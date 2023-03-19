a7521881b865a1e6a7b0cbd704aee50288ccc5e4
Pedro Gomes <p.f.q.gomes@gmail.com>
2023-03-19 20:43:11 +0000

    Now using AstroCommunity repository.

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,28 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      no_italic = true,
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        ts_rainbow2 = true,
+      },
+    },
+  },
+
+  { import = "astrocommunity.pack.rust" },
 }
