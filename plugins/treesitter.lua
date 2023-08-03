8336c83f3abb20a493a3bf0cd3ce76221f032896
Pedro Gomes <p.f.q.gomes@gmail.com>
2023-08-03 15:48:05 +0100

    Update treesitter configuration to include clang path.

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,15 +1,27 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = { "lua", "rust", "json", "toml", "c", "cpp" },
-    incremental_selection = {
-      enabled = true,
-      keymaps = {
-        init_selection = "<C-Space>",
-        node_incremental = "<C-Space>",
-        scope_incremental = "<nop>",
-        node_decremental = "<Backspace>",
-      }
-    },
-  },
+  build = ":TSUpdate",
+  config = function ()
+    require('nvim-treesitter.install').compilers = {'/opt/llvm-15.0_043/bin/clang'}
+    local configs = require("nvim-treesitter.configs")
+    
+    configs.setup({
+      ensure_installed = { "rust", "toml", "c", "cpp", "lua", "vim", "vimdoc", "javascript", "html" },
+      sync_install = false,
+      auto_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-Space>",
+          node_incremental = "<C-Space>",
+          scope_incremental = "<nop>",
+          node_decremental = "<Backspace>",
+        },
+      },
+      modules = {},
+      ignore_install = {},
+    })
+  end,
 }
